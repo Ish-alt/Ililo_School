09da995419d1708ce1ae34d9cfba275607f2fc5c
Ish-alt <ishaltaye@gmail.com>
2026-07-23 20:03:06 +0300

    Lesson collaps fix some chat bot and slide bar issues

--- data/lesson-list.js
+++ data/lesson-list.js
@@ -42,23 +42,91 @@ const G10 = [{
   id: "eng10",
   name: "English"
 }]
-const NG11 = []
-const SG11 = []
-const NG12 = []
-const SG12 = []
 
-export const G9_10 = [{
+const NG11 = [{
+  id: "phyN11",
+  name: "Physics"
+},{
+  id: "mathN11",
+  name: "Mathematics"
+},{
+  id: "bioN11",
+  name: "Biology"
+},{
+  id: "chemN11",
+  name: "Chemistry"
+},{
+  id: "engN11",
+  name: "English"
+}]
+const SG11 = [{
+  id: "histS11",
+  name: "History"
+},{
+  id: "geoS11",
+  name: "Geography"
+},{
+  id: "mathS11",
+  name: "Mathematics"
+},{
+  id: "ecoS11",
+  name: "Economics"
+},{
+  id: "engS11",
+  name: "English"
+}]
+
+const NG12 = [{
+  id: "phyN12",
+  name: "Physics"
+},{
+  id: "mathN12",
+  name: "Mathematics"
+},{
+  id: "bioN12",
+  name: "Biology"
+},{
+  id: "chemN12",
+  name: "Chemistry"
+},{
+  id: "engN12",
+  name: "English"
+}]
+const SG12 = [
+  {
+  id: "histS11",
+  name: "History"
+},{
+  id: "geoS11",
+  name: "Geography"
+},{
+  id: "mathS11",
+  name: "Mathematics"
+},{
+  id: "ecoS11",
+  name: "Economics"
+},{
+  id: "engS11",
+  name: "English"
+}
+]
+
+export const Grade = [{
   title: "Grade 9",
   grade: G9
 },{
   title: "Grade 10",
   grade: G10
-}]
-
-export const NG11_12 = [{
+},{
   title: "Grade 11",
   grade:  NG11
 },{
   title: "Grade 12",
   grade: NG12
+},{
+  title: "Grade 11",
+  grade:  SG11
+},{
+  title: "Grade 12",
+  grade: SG12
 }]
--- js/lesson.js
+++ js/lesson.js
@@ -1,19 +1,26 @@
-import { G9_10 } from "../data/lesson-list.js";
+import { Grade } from "../data/lesson-list.js";
 
-
-const g10Subjects = document.querySelector('.js-g10');
 const g11Subjects = document.querySelector('.js-g11');
 const g12Subjects = document.querySelector('.js-g12');
 
 let subjects = ""
 
-G9_10.forEach((grade) =>{
+Grade.forEach((grade) => {
   subjects += `
    <section class="Grade">
-      <p>${grade.title}</p>
+      <div class="title">
+        <p>${grade.title}</p>
+        <svg fill="var(--dark-color)" width="800px" height="800px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <g data-name="Layer 2">
+          <g data-name="chevron-down">
+          <rect width="24" height="24" opacity="0"/>
+          <path d="M12 15.5a1 1 0 0 1-.71-.29l-4-4a1 1 0 1 1 1.42-1.42L12 13.1l3.3-3.18a1 1 0 1 1 1.38 1.44l-4 3.86a1 1 0 0 1-.68.28z"/>
+          </g>
+          </g>
+        </svg>
+      </div>
       <div class="subject-wrapper">
-      ${
-        grade.grade.map((subject) =>`
+      ${grade.grade.map((subject) => `
           <div class="each-sub">
           <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="isolation:isolate" viewBox="0 0 2500 2500" width="2500" height="2500">
         <defs><clipPath id="clipPath"><rect width="2500" height="2500"/></clipPath></defs><g clip-path="url(#clipPath)"><g id="Group"><path d=" M 2205.8 547.4 L 1151.9 547.4 L 917.7 313.2 L 332.2 313.2 C 203.39 313.2 98 418.59 98 547.4 L 98 1015.8 L 2440 1015.8 L 2440 781.6 C 2440 652.79 2334.61 547.4 2205.8 547.4 Z "/><path d=" M 2205.8 547.4 L 332.2 547.4 C 203.39 547.4 98 652.79 98 781.6 L 98 1952.6 C 98 2081.41 203.39 2186.8 332.2 2186.8 L 2205.8 2186.8 C 2334.61 2186.8 2440 2081.41 2440 1952.6 L 2440 781.6 C 2440 652.79 2334.61 547.4 2205.8 547.4 Z "/></g></g>
@@ -21,11 +28,19 @@ G9_10.forEach((grade) =>{
           <p>${subject.name}</p>
         </div>
           ` ).join('')
-      }
-        
+    }
       </div>
     </section>
   `
 });
 
 document.querySelector('.lesson-page').innerHTML += subjects
+
+const collaps = document.querySelectorAll('.title')
+
+collaps.forEach((min) => {
+  min.addEventListener('click', () => {
+    const grade = min.closest('.Grade')
+    grade.classList.toggle('lesson-on')
+  })
+})
